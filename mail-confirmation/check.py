2e163830ffed9d066853bf89e62c4669ca60b887
Dmitry Filinov <dm.filinov@gmail.com>
2023-10-20 15:27:43 +0700

    added sending by email

--- mail-confirmation/check.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-response = requests.post("http://192.168.0.48:4121/mail-confirmation/api/v1.0/send-confirmation/", json={"user_id": 1})
-
-print(response.text)
-
-print(requests.post("http://192.168.0.48:4121/mail-confirmation/api/v1.0/check-confirmation/", json={"key": response.json()["key"]}).text)
--- mail-confirmation/src/api/urls_generator.py
+++ mail-confirmation/src/api/urls_generator.py
@@ -1,15 +1,24 @@
 import random
+import smtplib
 import string
 
 from datetime import datetime
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class URLGenerator(object):
     STRING = string.digits + string.ascii_letters
 
-    def __init__(self, login: str, password: str):
-        self.login = login
-        self.password = password
+    def __init__(self, email: str, password: str):
+        self.email = email
+        # self.password = password
+        self.server = smtplib.SMTP('smtp.gmail.com: 587')
+        self.server.starttls()
+        self.server.login(
+            email,
+            password
+        )
 
     def generate_url(self):
         url = ""
@@ -31,3 +40,19 @@ class URLGenerator(object):
         create_time = datetime.strptime(create_time, "%d.%m.%Y %H:%M:%S")
         create_time = (now - create_time)
         return int(create_time.seconds // 60) < 5
+
+    def send_key(self, email, key, link):
+        text = link + key
+        msg = MIMEMultipart()
+        msg['From'] = self.email
+        msg['To'] = email
+        msg['Subject'] = "Подтверждение email"
+
+        msg.attach(MIMEText(text))
+
+        self.server.sendmail(
+            msg['From'],
+            msg['To'],
+            msg.as_string()
+        )
+        # self.server.quit()
